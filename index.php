b59a09ddbc9efef9aeeb8ec1d3ea32cd70c8bb52
iPogXD <ipoghosyan@mail.ru>
2024-02-08 14:56:40 +0400

    fix of old commit

--- index.php
+++ index.php
@@ -3,7 +3,8 @@ error_reporting( E_ALL );
 ini_set( "display_errors",1);
 $color = "blue";
 $propStyle = "color:";
-
+$size = "24px";
+$fontSize = "font-size:";
 $html =
     "<!DOCTYPE html>
 <html lang='en'>
@@ -13,7 +14,8 @@ $html =
     <title>Document</title>
 </head>
 <body>
-    <p style='$propStyle  $color'>Test task</p>
+    <p style='$propStyle  $color; $fontSize $size'>Test task</p>
+    <h1>My first project demo</h1>
 </body>
 </html>";
 
